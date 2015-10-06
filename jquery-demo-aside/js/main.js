13f1dfb820bb255aef72298796b3af3c485f464a
Rei Lin <temisrei@gmail.com>
2015-10-06 22:43:19 +0800

    add jquery demo aside

---
+++ jquery-demo-aside/js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+	//
+	var duration = 600;
+
+	// aside
+	var $aside = $('.page-main > aside');
+	var $asidButton = $aside.find('button')
+		.on('click', function(){
+			$aside.toggleClass('open');
+			if($aside.hasClass('open')){
+				$aside.stop(true).animate({
+					left: '-50px'
+				}, duration, 'easeOutBack');
+				$asidButton.find('img').attr('src', './img/btn_close.png');
+			}
+			else{
+				$aside.stop(true).animate({
+					left: '-350px'
+				}, duration, 'easeInBack');
+				$asidButton.find('img').attr('src', './img/btn_open.png');
+			};
+		});
+});
